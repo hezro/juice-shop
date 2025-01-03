52f66bb9665812baf286a2090942f5b983617283
Orrin Rohrer <orrin.rohrer@gmail.com>
2025-01-03 10:10:59 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ require('./lib/startup/validateDependencies')().then(() => {
   const server = require('./server')
   server.start()
 })
+
+
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+
+app.configure(function () {
+  app.use(express.static(__dirname + '.'));
+});
